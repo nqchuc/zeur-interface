fae8b8bb7197f1cb70ae6f943f8e3fc5aab0397e
zexoverz <faisalfirdani01@gmail.com>
2025-06-20 18:38:32 +0700

    Types initialize and wagmi setup

---
+++ types/contracts.ts
@@ -0,0 +1,87 @@
+import { Address } from 'viem'
+
+export enum AssetType {
+  Collateral = 0,
+  Debt = 1
+}
+
+export interface StakedTokenData {
+  stakedToken: Address
+  underlyingAmount: bigint
+  stakedAmount: bigint
+}
+
+export interface AssetData {
+  assetType: AssetType
+  asset: Address
+  supplyCap: bigint
+  borrowCap: bigint
+  totalSupply: bigint
+  totalBorrow?: bigint
+  totalShares?: bigint
+  utilizationRate?: bigint
+  supplyRate?: bigint
+  borrowRate?: bigint
+  ltv?: number
+  liquidationThreshold?: number
+  liquidationBonus?: number
+  liquidationProtocolFee?: number
+  reserveFactor?: number
+  decimals: number
+  isFrozen: boolean
+  isPaused: boolean
+  stakedTokens?: StakedTokenData[]
+}
+
+export interface UserCollateralData {
+  collateralAsset: Address
+  supplyBalance: bigint
+}
+
+export interface UserDebtData {
+  debtAsset: Address
+  supplyBalance: bigint
+  borrowBalance: bigint
+}
+
+export interface UserData {
+  totalCollateralValue: bigint
+  totalDebtValue: bigint
+  availableBorrowsValue: bigint
+  currentLiquidationThreshold: bigint
+  ltv: bigint
+  healthFactor: bigint
+  userCollateralData: UserCollateralData[]
+  userDebtData: UserDebtData[]
+}
+
+// UI formatted types
+export interface FormattedAssetData {
+  assetType: AssetType
+  asset: Address
+  symbol: string
+  name: string
+  icon: string
+  supplyCap: string
+  borrowCap: string
+  totalSupply: string
+  totalBorrow: string
+  utilizationRate: string
+  supplyRate: string
+  borrowRate: string
+  decimals: number
+  isFrozen: boolean
+  isPaused: boolean
+  protocols: string[]
+  color: string
+}
+
+export interface FormattedUserDebtData {
+  debtAsset: Address
+  symbol: string
+  name: string
+  icon: string
+  supplyBalance: string
+  borrowBalance: string
+  netBorrow: string
+}
